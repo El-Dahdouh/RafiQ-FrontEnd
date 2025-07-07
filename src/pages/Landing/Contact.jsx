183c1866d5489484201b62a853cb68e270e423bf
LailaMosbah <lylymsbah2@gmail.com>
2025-07-07 18:33:59 +0300

    contact us page , enable sending messages

--- src/pages/Landing/Contact.jsx
+++ src/pages/Landing/Contact.jsx
@@ -1,6 +1,28 @@
-//import React from "react";
-
+import { useRef } from "react";
+import emailjs from "@emailjs/browser";
 const Contact = () => {
+  const form = useRef();
+  const sendMessage = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm("service_dujdxgk", "template_4kcnu5a", form.current, {
+        publicKey: "hab74yyQUVd2GMjdJ",
+      })
+      .then(
+        () => {
+          console.log("SUCCESS!");
+          document.getElementById("name").value = "";
+          document.getElementById("email").value = "";
+          document.getElementById("title").value = "";
+          document.getElementById("message").value = "";
+        },
+        (error) => {
+          console.log("FAILED...", error.text);
+        }
+      );
+  };
+
   return (
     <>
       <div className="contact-us px-4 py-10 bg-white">
@@ -14,30 +36,44 @@ const Contact = () => {
         </div>
 
         <div className="max-w-4xl mx-auto">
-          <form className="grid grid-cols-1 md:grid-cols-2 gap-6">
+          <form
+            ref={form}
+            onSubmit={sendMessage}
+            className="grid grid-cols-1 md:grid-cols-3 gap-6"
+          >
             <input
               type="text"
               id="name"
+              name="name"
               placeholder="Enter your name"
               className="p-3 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
             />
             <input
               type="email"
               id="email"
+              name="email"
               placeholder="Enter your email"
+              className="p-3 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+            />
+            <input
+              type="text"
+              id="title"
+              name="title"
+              placeholder="Enter a title for the message"
               className="p-3 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
             />
             <textarea
               id="message"
+              name="message"
               rows="4"
               placeholder="Enter your message"
-              className="p-3 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 md:col-span-2"
+              className="p-3 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 md:col-span-3"
             ></textarea>
             <button
               type="submit"
-              className="bg-blue-600 text-white py-3 px-6 rounded-md hover:bg-blue-700 transition duration-300 md:col-span-2"
+              className="bg-blue-600 text-white py-3 px-6 rounded-md hover:bg-blue-700 transition duration-300 md:col-span-3"
             >
-              Submit
+              send
             </button>
           </form>
         </div>
