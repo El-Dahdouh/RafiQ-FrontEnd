0aab1efae99abee4d0d7d3fe0b3a70255dd134b4
Esraa Soliman <ee341110@gmail.com>
2025-07-26 12:40:27 +0300

    fix footer

--- src/layouts/PrivateLayout.jsx
+++ src/layouts/PrivateLayout.jsx
@@ -2,14 +2,14 @@ import { Outlet } from "react-router-dom";
 import PrivateHeader from "./Header/PrivateHeader";
 import Footer from "./Footer";
 
-export default function PublicLayout() {
+export default function PrivateLayout() {
   return (
-    <>
+    <div className="flex flex-col min-h-screen">
       <PrivateHeader />
-      <main className="container mx-auto px-6 py-8">
+      <main className="container mx-auto px-6 py-8 flex-grow">
         <Outlet />
       </main>
       <Footer />
-    </>
+    </div>
   );
 }
--- src/layouts/PublicLayout.jsx
+++ src/layouts/PublicLayout.jsx
@@ -4,12 +4,12 @@ import Footer from "./Footer";
 
 export default function PublicLayout() {
   return (
-    <>
+    <div className="flex flex-col min-h-screen">
       <PublicHeader />
-      <main>
+      <main className="flex-grow">
         <Outlet />
       </main>
       <Footer />
-    </>
+    </div>
   );
 }
